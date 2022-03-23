b795272d475d85bdbbf36767959eeb08485d53c3
fuguhverdian <fahrulfuguhv@students.amikom.ac.id>
2022-03-23 20:17:01 +0700

    aplikasi taxi

---
+++ Class1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taxiaplication
+{
+    class Taxi
+    {
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public int NumPasangger { get; set; }
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine("DriverName: {0}", DriverName);
+            if (OnDuty == true)
+            {
+                Console.WriteLine("OnDuty = Yes");
+            }
+            else
+            {
+                Console.WriteLine("OnDuty = NO");
+            }
+            Console.WriteLine("Number of pasangger: {0}\n\n", NumPasangger);   
+        }
+        public void PickUpPasangger()
+        {
+            if (OnDuty == true)
+            {
+                Console.WriteLine("{0} sedang menjemput penumpang",DriverName);
+            }
+            else
+            {
+                return;
+            }
+        }
+        public void DropOffPasangger()
+        {
+            if (OnDuty == false)
+            {
+                Console.WriteLine("{0} selesai mengantar penumpang", DriverName);
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+ }
+
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taxiaplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Taxi taxi = new Taxi();
+            taxi.DriverName = "fahrul";
+            taxi.OnDuty = false;
+            taxi.NumPasangger = 10;
+
+            taxi.TaxiInfo();
+            taxi.PickUpPasangger();
+            taxi.DropOffPasangger();
+            Console.ReadKey();
+
+        }
+    }
+}
